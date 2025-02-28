cdf0d82482ff91770b1f7d363f60a01d1b4126a9
Srini235 <srinivasanr.98@outlook.com>
2025-02-28 21:16:42 +0530

    Modularity ensured

--- main.c
+++ main.c
@@ -1,49 +1,14 @@
 /* Includes */
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
+#include "main.h"
 
 /* Function prototypes */
-void binary_search_copilot(void);
-void binary_search_manual(void);
-uint8_t storeArrayData(uint8_t *array);
-void printArrayData(uint8_t search_index_u8, uint8_t *array);
-void binarySearch(void);
-uint8_t binarySearchNew(uint8_t num_elements_u8,uint8_t *array, uint8_t search_value_u8);
 
 int array_check[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 int main()
 {
-    //binary_search_copilot();
     binary_search_manual();
     return 0;
-}
-
-void binary_search_copilot(void)
-{
-    //binary search algorithm
-    int left = 0;   
-    int right = 9;
-    int mid;
-    int search = 5;
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
-        if (array_check[mid] == search)
-        {
-            printf("Element found at index %d\n", mid);
-            break;
-        }
-        else if (array_check[mid] < search)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-    }
 }
 
 uint8_t array_for_search[]; //array used for binary search
@@ -53,8 +18,6 @@ uint8_t searchStatus_u8;    //binary search status
 void binary_search_manual(void)
 {
     //Binary search algorithm in a generic way
-    // uint8_t left_index_u8 = 0;
-    // uint8_t right_index_u8 = 0;
      uint8_t search_index_u8 = 0;
 
     //Get array data to be searched
@@ -62,7 +25,6 @@ void binary_search_manual(void)
 
     //for debug purpose
     printArrayData(search_index_u8, array_for_search);
-
 
     //get value for search in array
     printf("Enter value for search: \n");
@@ -74,15 +36,12 @@ void binary_search_manual(void)
     {
         printf("Element not found\n");
     }
-
-
 }
 
 
 uint8_t storeArrayData(uint8_t *array)
 {
     //Note: This code has error, which has to be corrected. Unit test failure for value < 0 and value > 255
-
     uint8_t index_u8;
     for(index_u8 = 0; index_u8 < 10; index_u8++)
     {
